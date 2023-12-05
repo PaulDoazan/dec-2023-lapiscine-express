036bc4c01a247b4f6502c19130ca14281a72124d
pdoazan <paul.doazan@gmail.com>
2023-12-05 10:13:44 +0100

    refactor delete and put coworkings endpoints, START DAY 6

--- routes/coworkingRoutes.js
+++ routes/coworkingRoutes.js
@@ -43,39 +43,34 @@ router
             })
     })
     .put((req, res) => {
-        Coworking.update(req.body, {
-            where: {
-                id: req.params.id
-            }
-        })
+        Coworking.findByPk(req.params.id)
             .then((result) => {
-                // La valeur result retournée par la méthode update() est le nombre de lignes modifiées dans la table Coworkings
-                if (result > 0) {
-                    Coworking.findByPk(req.params.id)
-                        .then((coworking) => {
-                            res.json({ message: 'Le coworking a bien été mis à jour.', data: coworking })
+                if (result) {
+                    result.update(req.body)
+                        .then(() => {
+                            res.json({ message: 'Le coworking a bien été mis à jour.', data: result })
                         })
                         .catch(error => {
-                            res.json({ message: 'Une erreur est survenue.', data: error.message })
+                            res.json({ message: 'La mise à jour a échoué.', data: error.message })
                         })
                 } else {
                     res.json({ message: `Aucun coworking n'a été mis à jour.` })
                 }
             })
             .catch(error => {
-                res.json({ message: 'La mise à jour a échoué.', data: error.message })
+                res.json({ message: 'Une erreur est survenue.', data: error.message })
             })
     })
     .delete((req, res) => {
         // A. On vérifie que l'id passé en req.params.id renvoie bien une ligne de notre table.
         Coworking.findByPk(req.params.id)
-            .then((coworking) => {
+            .then((result) => {
                 // B. Si un coworking correspond à l'id alors on exécute la méthode destroy()
-                if (coworking) {
-                    Coworking.destroy({ where: { id: req.params.id } })
+                if (result) {
+                    result.destroy()
                         // C. Si le coworking est bien supprimé, on affiche un message avec comme data le coworking récupéré dans le .findByPk()
-                        .then(() => {
-                            res.json({ mesage: `Le coworking a bien été supprimé.`, data: coworking })
+                        .then((result) => {
+                            res.json({ mesage: `Le coworking a bien été supprimé.`, data: result })
                         })
                         // D. Si la suppression a échoué, on retourne une réponse à POSTMAN
                         .catch((error) => {
