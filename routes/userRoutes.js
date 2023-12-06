edd8ba9823ab349c99cd0fc2e53dfab40c58c885
pdoazan <paul.doazan@gmail.com>
2023-12-06 17:05:43 +0100

    Add jsonwebtoken for authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const { findAllUsers, findUserByPk, createUser, updateUser, deleteUser } = require('../controllers/userControllers')
+const { login } = require('../controllers/authControllers')
 
 router
     .route('/')
     .get(findAllUsers)
     .post(createUser)
+
+router
+    .route('/login')
+    .post(login)
 
 router
     .route('/:id')
